88b431d4057dc3670dea469d137504e62c5948ad
Andrew Leader <followthatleader@outlook.com>
2019-11-03 21:17:19 -0800

    Droid support for different first day of week

--- PowerPlannerAndroid/MainActivity.cs
+++ PowerPlannerAndroid/MainActivity.cs
@@ -334,6 +334,11 @@ namespace PowerPlannerAndroid
                 {
                     string changedText = "";
 
+                    if (v < new Version(1911, 2, 0, 0))
+                    {
+                        changedText += "\n - Support for Monday as first day of week on Calendar for countries like Spain!";
+                    }
+
                     if (v < new Version(5, 4, 86, 0))
                     {
                         changedText += "\n - 2x improvement in app launch speed!";
