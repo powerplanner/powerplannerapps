2c74cb9a4a0d35637d0baa6976bbfa9f206c06a0
Andrew Leader <followthatleader@outlook.com>
2020-05-29 14:13:43 -0700

    iOS snackbar works

--- PowerPlanneriOS/App/PowerPlanneriOSApp.cs
+++ PowerPlanneriOS/App/PowerPlanneriOSApp.cs
@@ -8,6 +8,8 @@ using UIKit;
 using PowerPlannerAppDataLibrary.App;
 using BareMvvm.Core.App;
 using System.Threading.Tasks;
+using InterfacesiOS.Views;
+using PowerPlanneriOS.Helpers;
 
 namespace PowerPlanneriOS.App
 {
@@ -27,6 +29,8 @@ namespace PowerPlanneriOS.App
 
         protected override Task InitializeAsyncOverride()
         {
+            BareSnackbarPresenter.ButtonTextColor = ColorResources.PowerPlannerAccentBlue;
+
             PowerPlannerAppDataLibrary.SyncLayer.SyncExtensions.GetAppName = delegate { return "Power Planner for iOS"; };
 
             // Note that there's several places my code takes a dependency on this to change behavior for iOS version
