062865b3fc63f6ba1ceab6aa3d91961712b16fbc
Andrew Glick <17516195+Antyos@users.noreply.github.com>
2021-02-02 14:24:14 -0600

    Added KeyPressed helper for Ctrl key

---
+++ PowerPlannerUWP/Helpers/KeyPressedHelpers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.System;
+using Windows.UI.Core;
+
+namespace PowerPlannerUWP.Helpers
+{
+    static class KeyPressedHelpers
+    {
+        // From: https://docs.microsoft.com/en-us/windows/uwp/design/input/keyboard-events#shortcut-keys-example
+        /// <summary>
+        /// Is the "Control" key pressed (held down)?
+        /// </summary>
+        /// <returns>True if Ctrl key is held down</returns>
+        public static bool IsCtrlKeyPressed()
+        {
+            var ctrlState = CoreWindow.GetForCurrentThread().GetKeyState(VirtualKey.Control);
+            return (ctrlState & CoreVirtualKeyStates.Down) == CoreVirtualKeyStates.Down;
+        }
+    }
+}
