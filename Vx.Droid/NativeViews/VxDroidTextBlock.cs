367a73588df14e556800dd509d664abfbd171c17
Andrew Leader <followthatleader@outlook.com>
2021-03-30 07:49:13 -0700

    Switch to appcompattextview

--- Vx.Droid/NativeViews/VxDroidTextBlock.cs
+++ Vx.Droid/NativeViews/VxDroidTextBlock.cs
@@ -4,6 +4,7 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using AndroidX.AppCompat.Widget;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,7 +13,7 @@ using Vx.Views;
 
 namespace Vx.Droid.NativeViews
 {
-    public class VxDroidTextBlock : VxDroidNativeView<VxTextBlock, TextView>, IVxTextBlock
+    public class VxDroidTextBlock : VxDroidNativeView<VxTextBlock, AppCompatTextView>, IVxTextBlock
     {
         public string Text { set => NativeView.Text = value; }
     }
