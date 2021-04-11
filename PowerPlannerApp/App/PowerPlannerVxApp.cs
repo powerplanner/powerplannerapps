e78df220e2de8e21689bafa109b2cb488865004c
Andrew Leader <followthatleader@outlook.com>
2021-04-10 21:16:01 -0700

    Small fixes for Vx PowerPlannerApp

--- PowerPlannerApp/App/PowerPlannerVxApp.cs
+++ PowerPlannerApp/App/PowerPlannerVxApp.cs
@@ -5,6 +5,7 @@ using PowerPlannerApp.Pages.WelcomePages;
 using PowerPlannerApp.SyncLayer;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
