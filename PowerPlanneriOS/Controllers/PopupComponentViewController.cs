1782eebe76382d5cb9317a36e526f9241ab4705f
Andrew Leader <followthatleader@outlook.com>
2021-05-14 14:07:35 -0700

    Fix iOS bottom margin

--- PowerPlanneriOS/Controllers/PopupComponentViewController.cs
+++ PowerPlanneriOS/Controllers/PopupComponentViewController.cs
@@ -3,6 +3,7 @@ using BareMvvm.Core.ViewModels;
 using InterfacesiOS.Controllers;
 using InterfacesiOS.Views;
 using PowerPlannerAppDataLibrary.ViewModels;
+using PowerPlannerAppDataLibrary.ViewModels.MainWindow.Settings;
 using ToolsPortable;
 using UIKit;
 using Vx.iOS;
@@ -11,6 +12,7 @@ namespace PowerPlanneriOS.Controllers
 {
     public class PopupComponentViewController : BareMvvmUIViewController<PopupComponentViewModel>
     {
+        private object _tabBarHeightListener;
         public override void OnViewModelSetOverride()
         {
             base.OnViewModelSetOverride();
@@ -40,6 +42,16 @@ namespace PowerPlanneriOS.Controllers
             renderedComponent.TranslatesAutoresizingMaskIntoConstraints = false;
             View.Add(renderedComponent);
             renderedComponent.StretchWidthAndHeight(View);
+
+            if (ViewModel.FindAncestor<SettingsViewModel>() != null)
+            {
+                // Accomodate for bottom bar when in settings
+                MainScreenViewController.ListenToTabBarHeightChanged(ref _tabBarHeightListener, delegate
+                {
+                    View.RemoveConstraints(View.Constraints);
+                    renderedComponent.StretchWidthAndHeight(View, 0, 0, 0, (float)MainScreenViewController.TAB_BAR_HEIGHT);
+                });
+            }
         }
 
         private void PrimaryButton_Clicked(object sender, EventArgs e)
