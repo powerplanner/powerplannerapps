446bcce75bcda2c509cdc5a29cfb1c1671e763d8
Andrew Leader <followthatleader@outlook.com>
2021-07-17 15:34:44 -0700

    Mostly finished iOS time picker

--- Vx.iOS/Views/iOSTimePicker.cs
+++ Vx.iOS/Views/iOSTimePicker.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using InterfacesiOS.Helpers;
 using UIKit;
 using Vx.Views;
 
@@ -7,38 +6,53 @@ namespace Vx.iOS.Views
 {
     public class iOSTimePicker : iOSView<Vx.Views.TimePicker, UIView>
     {
-        private UILabel _text;
+        private UILabel _header;
+        private UIDatePicker _datePicker;
 
         public iOSTimePicker()
         {
-            UITapGestureRecognizer tapRecognizer = new UITapGestureRecognizer();
-
-            tapRecognizer.AddTarget(() => ShowTimePicker());
-
-            View.AddGestureRecognizer(tapRecognizer);
-
-            _text = new UILabel()
+            _header = new UILabel
             {
                 TranslatesAutoresizingMaskIntoConstraints = false
             };
-            View.Add(_text);
-            _text.StretchWidthAndHeight(View, 6, 6, 6, 6);
 
-            View.BackgroundColor = UIColorCompat.SystemGroupedBackgroundColor;
-            View.Layer.CornerRadius = 10;
-            View.ClipsToBounds = true;
+            _datePicker = new UIDatePicker
+            {
+                TranslatesAutoresizingMaskIntoConstraints = false,
+                Mode = UIDatePickerMode.Time,
+                PreferredDatePickerStyle = UIDatePickerStyle.Inline
+            };
+            _datePicker.ValueChanged += View_ValueChanged;
+
+            View.AddSubview(_header);
+            View.AddSubview(_datePicker);
+
+            _header.StretchWidth(View);
+            //_datePicker.StretchWidth(View);
+            View.AddConstraints(NSLayoutConstraint.FromVisualFormat("H:|[datePicker]->=0-|", NSLayoutFormatOptions.DirectionLeadingToTrailing,
+                "datePicker", _datePicker));
+
+            View.AddConstraints(NSLayoutConstraint.FromVisualFormat("V:|[header][datePicker]|", NSLayoutFormatOptions.DirectionLeadingToTrailing,
+                "header", _header,
+                "datePicker", _datePicker));
         }
 
-        private void ShowTimePicker()
+        private void View_ValueChanged(object sender, EventArgs e)
         {
-            // TODO
+            var newTime = BareUIHelper.NSDateToDateTime(_datePicker.Date).TimeOfDay;
+
+            if (newTime != VxView.Value && VxView.ValueChanged != null)
+            {
+                VxView.ValueChanged(newTime);
+            }
         }
 
         protected override void ApplyProperties(TimePicker oldView, TimePicker newView)
         {
             base.ApplyProperties(oldView, newView);
 
-            _text.Text = DateTime.Today.Add(newView.Value).ToString("t");
+            _header.Text = newView.Header;
+            _datePicker.Date = BareUIHelper.DateTimeToNSDate(DateTime.Today.Add(newView.Value));
         }
     }
 }
