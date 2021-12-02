0cbe691021dbbaee48bf52538682b5158bdb3ecc
Andrew Leader <followthatleader@outlook.com>
2021-12-02 11:28:49 -0800

    Droid .NET 6 compiles
    But it seems to crash on launch...

--- PowerPlannerAndroid/Extensions/DroidEmailExtension.cs
+++ PowerPlannerAndroid/Extensions/DroidEmailExtension.cs
@@ -8,7 +8,6 @@ using PowerPlannerAppDataLibrary.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
 using Vx.Droid;
