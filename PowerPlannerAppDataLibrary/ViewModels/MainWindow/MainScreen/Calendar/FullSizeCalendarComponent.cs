3d40cde35580d37bcc3ea26d1ffa9e54fb0d267a
Andrew Leader <followthatleader@outlook.com>
2021-12-15 18:39:04 -0800

    Added some more to calendar

--- PowerPlannerAppDataLibrary/ViewModels/MainWindow/MainScreen/Calendar/FullSizeCalendarComponent.cs
+++ PowerPlannerAppDataLibrary/ViewModels/MainWindow/MainScreen/Calendar/FullSizeCalendarComponent.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using PowerPlannerAppDataLibrary.ViewItems.BaseViewItems;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using ToolsPortable;
 using Vx.Views;
@@ -43,10 +45,103 @@ namespace PowerPlannerAppDataLibrary.ViewModels.MainWindow.MainScreen.Calendar
         {
             var month = _thisMonth.AddMonths(position);
 
-            return new TextBlock
+            return new FullSizeCalendarMonthComponent
             {
-                Text = month.ToString()
+                Month = month,
+                FirstDayOfWeek = _viewModel.FirstDayOfWeek,
+                Items = _viewModel.SemesterItemsViewGroup.Items
             };
+        }
+
+        private class FullSizeCalendarMonthComponent : VxComponent
+        {
+            public DateTime Month { get; set; }
+            public DayOfWeek FirstDayOfWeek { get; set; }
+            public MyObservableList<BaseViewItemMegaItem> Items { get; set; }
+
+            protected override View Render()
+            {
+                SubscribeToCollection(Items);
+
+                DateTime[,] array = CalendarArray.Generate(Month, FirstDayOfWeek);
+
+                var dayHeaders = new LinearLayout
+                {
+                    Orientation = Orientation.Horizontal
+                };
+
+                for (int i = 0; i < 7; i++)
+                {
+                    dayHeaders.Children.Add(new TextBlock
+                    {
+                        Text = DateTools.ToLocalizedString(FirstDayOfWeek + i)
+                    }.LinearLayoutWeight(1));
+                }
+
+                var grid = new LinearLayout();
+
+                for (int r = 0; r < 6; r++)
+                {
+                    var row = new LinearLayout
+                    {
+                        Orientation = Orientation.Horizontal
+                    };
+
+                    for (int c = 0; c < 7; c++)
+                    {
+                        var date = array[r, c];
+                        DayType dayType;
+
+                        if (DateTools.SameMonth(date, Month))
+                        {
+                            dayType = DayType.ThisMonth;
+                        }
+                        else if (date < Month)
+                        {
+                            dayType = DayType.PrevMonth;
+                        }
+                        else
+                        {
+                            dayType = DayType.NextMonth;
+                        }
+
+                        var itemsOnDay = Items.Where(i => i.Date.Date == date).ToArray();
+
+                        row.Children.Add(new Border
+                        {
+                            Content = new TextBlock
+                            {
+                                Text = date.Day.ToString() + " - " + itemsOnDay.Length
+                            }
+                        }.LinearLayoutWeight(1));
+                    }
+
+                    grid.Children.Add(row.LinearLayoutWeight(1));
+                }
+
+                return new LinearLayout
+                {
+                    Children =
+                    {
+                        new TextBlock
+                        {
+                            Text = Month.ToString(),
+                            Margin = new Thickness(24)
+                        }.TitleStyle(),
+
+                        dayHeaders,
+
+                        grid.LinearLayoutWeight(1)
+                    }
+                };
+            }
+
+            public enum DayType
+            {
+                ThisMonth,
+                PrevMonth,
+                NextMonth
+            }
         }
     }
 }
