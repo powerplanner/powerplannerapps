46eb5dab5af3cd564be079c902cd93ee47e0a532
Andrew Leader <followthatleader@outlook.com>
2021-12-16 17:01:47 -0800

    Remove commented out code

--- PowerPlannerAppDataLibrary/ViewModels/MainWindow/MainScreen/Calendar/FullSizeCalendarComponent.cs
+++ PowerPlannerAppDataLibrary/ViewModels/MainWindow/MainScreen/Calendar/FullSizeCalendarComponent.cs
@@ -35,15 +35,6 @@ namespace PowerPlannerAppDataLibrary.ViewModels.MainWindow.MainScreen.Calendar
                 Position = VxValue.Create(DateTools.DifferenceInMonths(_viewModel.DisplayMonth, _thisMonth), i => _viewModel.DisplayMonth = _thisMonth.AddMonths(i)),
                 ItemTemplate = _itemTemplate
             };
-
-            //return new SlideView
-            //{
-            //    CurrentContent = RenderContent(_viewModel.DisplayMonth),
-            //    PreviousContent = RenderContent(_viewModel.DisplayMonth.AddMonths(-1)),
-            //    NextContent = RenderContent(_viewModel.DisplayMonth.AddMonths(1)),
-            //    OnMovedNext = () => _viewModel.DisplayMonth = _viewModel.DisplayMonth.AddMonths(1),
-            //    OnMovedPrevious = () => _viewModel.DisplayMonth = _viewModel.DisplayMonth.AddMonths(-1)
-            //};
         }
 
         private View RenderContent(int position)
