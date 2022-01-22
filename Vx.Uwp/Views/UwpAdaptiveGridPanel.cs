6ba50e14213a0581b30e0cf09b23b14b7ff98a41
Andrew Leader <aleader@microsoft.com>
2022-01-22 14:12:14 -0800

    Switch UWP to actual AdaptiveGridPanel (#100)

---
+++ Vx.Uwp/Views/UwpAdaptiveGridPanel.cs
@@ -0,0 +1,28 @@
+﻿using InterfacesUWP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vx.Views;
+
+namespace Vx.Uwp.Views
+{
+    public class UwpAdaptiveGridPanel : UwpView<Vx.Views.AdaptiveGridPanel, MyAdaptiveGridPanel>
+    {
+        public UwpAdaptiveGridPanel()
+        {
+            View.StretchIfOnlyOneChild = true;
+        }
+
+        protected override void ApplyProperties(AdaptiveGridPanel oldView, AdaptiveGridPanel newView)
+        {
+            base.ApplyProperties(oldView, newView);
+
+            View.MinColumnWidth = newView.MinColumnWidth;
+            View.ColumnSpacing = newView.ColumnSpacing;
+
+            ReconcileList(oldView?.Children, newView.Children, View.Children);
+        }
+    }
+}
