7aa8e28fb3a485eb6b0708c59afdb2513948203a
Andrew Leader <followthatleader@outlook.com>
2024-08-28 16:32:45 -0700

    Update UIApplication.Main to new method

--- PowerPlanneriOS/AppDelegate.cs
+++ PowerPlanneriOS/AppDelegate.cs
@@ -38,7 +38,6 @@ namespace PowerPlanneriOS
     // The UIApplicationDelegate for the application. This class is responsible for launching the 
     // User Interface of the application, as well as listening (and optionally responding) to 
     // application events from iOS.
-    [Register("AppDelegate")]
     public class AppDelegate : NativeiOSApplication
     {
         private MainAppWindow _mainAppWindow;
