97206a7ec0d786244703fb2148973c9ea22d45f1
Andrew Leader <followthatleader@outlook.com>
2026-06-11 00:16:08 -0700

    Mac builds

--- PowerPlanneriOS/Bridge.swift
+++ PowerPlanneriOS/Bridge.swift
@@ -1,8 +1,12 @@
+#if !targetEnvironment(macCatalyst)
 import WidgetKit
+#endif
 
 @_cdecl("ReloadWidgets")
 public func ReloadWidgets() {
+    #if !targetEnvironment(macCatalyst)
     if #available(iOS 14, *) {
         WidgetCenter.shared.reloadAllTimelines()
     }
+    #endif
 }
