64578b47f67ed16ff6243a54bac08b1449fb9725
Andrew Leader <followthatleader@outlook.com>
2026-06-12 02:30:06 -0700

    Enable reload widgets

--- PowerPlanneriOS/Bridge.swift
+++ PowerPlanneriOS/Bridge.swift
@@ -1,12 +1,8 @@
-#if !targetEnvironment(macCatalyst)
 import WidgetKit
-#endif
 
 @_cdecl("ReloadWidgets")
 public func ReloadWidgets() {
-    #if !targetEnvironment(macCatalyst)
     if #available(iOS 14, *) {
         WidgetCenter.shared.reloadAllTimelines()
     }
-    #endif
 }
